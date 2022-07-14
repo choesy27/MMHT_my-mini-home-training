e926e1bf65da2ae8915568beacf3616176846bd6
choesy27 <choesy@suwon.ac.kr>
2022-07-14 16:30:33 +0900

    First commit - 마미홈트 프로젝트 생성

---
+++ app/src/main/java/com/example/mmht_my_mini_home_training/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mmht_my_mini_home_training;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
