3eb0688a990107fc1c7d5c6f3cf1fa66aa8953db
choesy27 <choesy@suwon.ac.kr>
2022-07-14 17:26:21 +0900

    Add Bottom Navigation - 하단 네비게이션 추가 - 메뉴 '캘린더'는 변동 가능성 있음.

--- app/src/main/java/com/example/mmht_my_mini_home_training/MainActivity.java
+++ app/src/main/java/com/example/mmht_my_mini_home_training/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.mmht_my_mini_home_training;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private HomeFragment homeFragment;
+    private CalendarFragment calendarFragment;
+    private SettingFragment settingFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.menu_home:
+                    setFragment(0);
+
+                default:
+                    break;
+
+                case R.id.menu_calendar:
+                    setFragment(1);
+                    break;
+
+                case R.id.menu_setting:
+                    setFragment(2);
+                    break;
+            }
+            return true;
+        });
+
+        homeFragment = new HomeFragment();
+        calendarFragment = new CalendarFragment();
+        settingFragment = new SettingFragment();
+
+        setFragment(0);     // 첫 프래그먼트 화면 지정
+    }
+
+    // 프래그먼트 교체
+    private void setFragment(int n) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch (n) {
+            case 0:
+                fragmentTransaction.replace(R.id.mainFrame, homeFragment);
+                fragmentTransaction.commit();
+                break;
+
+            case 1:
+                fragmentTransaction.replace(R.id.mainFrame, calendarFragment);
+                fragmentTransaction.commit();
+                break;
+
+            case 2:
+                fragmentTransaction.replace(R.id.mainFrame, settingFragment);
+                fragmentTransaction.commit();
+                break;
+
+            default:
+                break;
+        }
     }
 }
